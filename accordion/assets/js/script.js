eea0be20378a7ba4c532d2e8986d59c9bfdac0d8
Sahar Momeni <saharmomo8419@gmail.com>
2026-02-24 23:40:54 +0330

    Add files via upload

---
+++ accordion/assets/js/script.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll(".title");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
