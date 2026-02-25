e8eff7f74cc1d85df978a37c185b4f1d84c42498
Sahar Momeni <saharmomo8419@gmail.com>
2026-02-25 23:26:34 +0330

    Add files via upload

---
+++ counter/assets/js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+
+    
+    if (num === end) {
+        alert("Start and end numbers should not be equal");
+        return;
+    }
+
+    num = document.getElementById("counter").textContent;
+
+    const interval = setInterval(() => {
+
+        if (num < end) {
+            num++;
+        } else if (num > end) {
+            num--;
+        }
+
+        num = document.getElementById("counter").textContent;
+
+        if (num === end) {
+            clearInterval(interval);
+        }
+    }, speed);
+});
+
+let num = Number(document.getElementById("startValue").value);
+let end = Number(document.getElementById("endValue").value);
+let speed = Number(document.getElementById("speedValue").value);
